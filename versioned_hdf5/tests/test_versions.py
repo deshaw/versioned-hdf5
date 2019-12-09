9041260c5d50ad448d1b97f9f70dc58e3d05b638
Aaron Meurer <asmeurer@gmail.com>
2019-12-09 15:00:09 -0700

    Add some code to write more data and build a virtual dataset
    Everything still only works with one dimension. It also assumes everything is
    a multiple of the chunk size. Finally, it doesn't yet reuse data chunks if
    they already exist in the raw data.

--- versioned_hdf5/tests/test_versions.py
+++ versioned_hdf5/tests/test_versions.py
@@ -1,10 +1,43 @@
 # TODO: Use a fixture for the test file
 import h5py
 import numpy as np
+from numpy.testing import assert_equal
 
-from ..versions import create_base_dataset, initialize
+from ..versions import (create_base_dataset, initialize, write_dataset, create_virtual_dataset, CHUNK_SIZE)
 
-def test_create_base_dataset():
+def setup():
     f = h5py.File('test.hdf5', 'w')
     initialize(f)
-    create_base_dataset(f, 'test_data', data=np.ones((1024,)))
+    return f
+
+def test_initialize():
+    with setup():
+        pass
+
+def test_create_base_dataset():
+    with setup() as f:
+        create_base_dataset(f, 'test_data', data=np.ones((CHUNK_SIZE,)))
+
+def test_write_dataset():
+    with setup() as f:
+        write_dataset(f, 'test_data', np.ones((CHUNK_SIZE,)))
+        write_dataset(f, 'test_data', 2*np.ones((CHUNK_SIZE,)))
+        write_dataset(f, 'test_data', 3*np.ones((CHUNK_SIZE,)))
+
+        ds = f['/_version_data/raw_data/test_data']
+        assert ds.shape == (3*CHUNK_SIZE,)
+        assert_equal(ds[0:CHUNK_SIZE], 1.0)
+        assert_equal(ds[CHUNK_SIZE], 2.0)
+        assert_equal(ds[2*CHUNK_SIZE], 3.0)
+
+def test_create_virtual_dataset():
+    with setup() as f:
+        write_dataset(f, 'test_data', np.ones((CHUNK_SIZE,)))
+        write_dataset(f, 'test_data', 2*np.ones((CHUNK_SIZE,)))
+        write_dataset(f, 'test_data', 3*np.ones((CHUNK_SIZE,)))
+
+        virtual_data = create_virtual_dataset(f, 'test_data', (2*CHUNK_SIZE,), [0, 2])
+
+        assert virtual_data.shape == (2*CHUNK_SIZE,)
+        assert_equal(virtual_data[0:CHUNK_SIZE], 1.0)
+        assert_equal(virtual_data[CHUNK_SIZE:2*CHUNK_SIZE], 3.0)
--- versioned_hdf5/versions.py
+++ versioned_hdf5/versions.py
@@ -1,3 +1,5 @@
+from h5py import VirtualLayout, VirtualSource
+
 CHUNK_SIZE = 2**20
 
 
@@ -8,20 +10,38 @@ def get_chunks(shape):
     return (CHUNK_SIZE,)
 
 def initialize(f):
-    f.create_group('_version_data')
+    f.create_group('_version_data/raw_data')
 
 def create_base_dataset(f, name, *, shape=None, data=None):
     if data is not None and shape is not None:
         raise ValueError("Only one of data or shape should be passed")
     if shape is None:
         shape = data.shape
-    ds = f['/_version_data'].create_dataset(name, shape=shape, data=data,
+    ds = f['/_version_data/raw_data'].create_dataset(name, shape=shape, data=data,
                                             chunks=get_chunks(shape),
                                             maxshape=(None,)*len(shape))
 
     return ds
 
 def write_dataset(f, name, data):
-    if name not in f['/_version_data']:
+    if name not in f['/_version_data/raw_data']:
         create_base_dataset(f, name, data=data)
         return
+
+    ds = f['/_version_data/raw_data'][name]
+    # TODO: Handle more than one dimension
+    old_shape = ds.shape
+    ds.resize((old_shape[0] + data.shape[0],))
+    ds[old_shape[0]:] = data
+    return ds
+
+def create_virtual_dataset(f, name, shape, indices):
+    layout = VirtualLayout(shape)
+    vs = VirtualSource(f['_version_data/raw_data'][name])
+
+    for i, idx in enumerate(indices):
+        # TODO: This needs to handle more than one dimension
+        layout[i*CHUNK_SIZE:(i+1)*CHUNK_SIZE] = vs[idx*CHUNK_SIZE:(idx+1)*CHUNK_SIZE]
+
+    virtual_data = f.create_virtual_dataset(name, layout)
+    return virtual_data
