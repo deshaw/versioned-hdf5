6d8cae130f5dae5d5753171696d647d3ce4c1321
Aaron Meurer <asmeurer@gmail.com>
2019-12-18 14:59:00 -0700

    Have the virtual dataset tests test duplicated chunks

--- versioned_hdf5/tests/test_versions.py
+++ versioned_hdf5/tests/test_versions.py
@@ -66,7 +66,7 @@ def test_write_dataset_offset():
 
 def test_create_virtual_dataset():
     with setup() as f:
-        slices1 = write_dataset(f, 'test_data', np.ones((CHUNK_SIZE,)))
+        slices1 = write_dataset(f, 'test_data', np.ones((2*CHUNK_SIZE,)))
         slices2 = write_dataset(f, 'test_data',
                                 np.concatenate((2*np.ones((CHUNK_SIZE,)),
                                                 3*np.ones((CHUNK_SIZE,)))))
@@ -74,14 +74,14 @@ def test_create_virtual_dataset():
         virtual_data = create_virtual_dataset(f, 'test_data',
                                               slices1 + [slices2[1]])
 
-        assert virtual_data.shape == (2*CHUNK_SIZE,)
-        assert_equal(virtual_data[0:CHUNK_SIZE], 1.0)
-        assert_equal(virtual_data[CHUNK_SIZE:2*CHUNK_SIZE], 3.0)
+        assert virtual_data.shape == (3*CHUNK_SIZE,)
+        assert_equal(virtual_data[0:2*CHUNK_SIZE], 1.0)
+        assert_equal(virtual_data[2*CHUNK_SIZE:3*CHUNK_SIZE], 3.0)
 
 
 def test_create_virtual_dataset_offset():
     with setup() as f:
-        slices1 = write_dataset(f, 'test_data', np.ones((CHUNK_SIZE,)))
+        slices1 = write_dataset(f, 'test_data', np.ones((2*CHUNK_SIZE,)))
         slices2 = write_dataset(f, 'test_data',
                                 np.concatenate((2*np.ones((CHUNK_SIZE,)),
                                                 3*np.ones((CHUNK_SIZE - 2,)))))
@@ -89,6 +89,6 @@ def test_create_virtual_dataset_offset():
         virtual_data = create_virtual_dataset(f, 'test_data',
                                               slices1 + [slices2[1]])
 
-        assert virtual_data.shape == (2*CHUNK_SIZE - 2,)
-        assert_equal(virtual_data[0:CHUNK_SIZE], 1.0)
-        assert_equal(virtual_data[CHUNK_SIZE:2*CHUNK_SIZE - 2], 3.0)
+        assert virtual_data.shape == (3*CHUNK_SIZE - 2,)
+        assert_equal(virtual_data[0:2*CHUNK_SIZE], 1.0)
+        assert_equal(virtual_data[2*CHUNK_SIZE:3*CHUNK_SIZE - 2], 3.0)
