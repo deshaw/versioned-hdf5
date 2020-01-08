87c9c479d375d4262ff899fa8fc5add968d966a9
Aaron Meurer <asmeurer@gmail.com>
2020-01-08 16:33:37 -0700

    Make the test_data.py file runnable

--- test_data.py
+++ test_data.py
@@ -9,7 +9,7 @@ from unittest import TestCase
 import h5py
 import numpy as np
 
-from .utils import temp_dir_ctx
+# from .utils import temp_dir_ctx
 
 
 class TestVersionedDatasetPerformance(TestCase):
@@ -62,26 +62,26 @@ class TestVersionedDatasetPerformance(TestCase):
                                    pct_inserts, num_inserts):
         logger = logging.getLogger(__name__)
 
-        with temp_dir_ctx() as tmp_dir:
-            filename = tmp_dir + '/test_versioned_dataset_performance.h5'
-            tts = []
-            f = h5py.File(filename, 'w')
-            try:
-                key0_ds = f.create_dataset(name + '/key0', data=np.random.rand(num_rows_initial),
-                                           dtype=(np.dtype('int64')), maxshape=(None,), chunks=(int(1e4),))
-                key1_ds = f.create_dataset(name + '/key1', data=np.random.rand(num_rows_initial),
-                                           dtype=(np.dtype('int64')), maxshape=(None,), chunks=(int(1e4),))
-                val_ds = f.create_dataset(name + '/val', data=np.random.rand(num_rows_initial),
-                                          dtype=(np.dtype('float64')), maxshape=(None,), chunks=(int(1e4),))
-                for a in range(num_transactions):
-                    tt = cls._modify_dss_sparse(key0_ds, key1_ds, val_ds, num_rows_per_append,
-                                                pct_changes if a > 0 else 0.0, num_changes,
-                                                pct_deletes if a > 0 else 0.0, num_deletes,
-                                                pct_inserts if a > 0 else 0.0, num_inserts)
-                    tts.append(tt)
-                    logger.info('Wrote transaction %d at transaction time %s', a, tt)
-            finally:
-                f.close()
+        tmp_dir = '.'
+        filename = tmp_dir + f'/{name}.h5'
+        tts = []
+        f = h5py.File(filename, 'w')
+        try:
+            key0_ds = f.create_dataset(name + '/key0', data=np.random.rand(num_rows_initial),
+                                       dtype=(np.dtype('int64')), maxshape=(None,), chunks=(int(1e4),))
+            key1_ds = f.create_dataset(name + '/key1', data=np.random.rand(num_rows_initial),
+                                       dtype=(np.dtype('int64')), maxshape=(None,), chunks=(int(1e4),))
+            val_ds = f.create_dataset(name + '/val', data=np.random.rand(num_rows_initial),
+                                      dtype=(np.dtype('float64')), maxshape=(None,), chunks=(int(1e4),))
+            for a in range(num_transactions):
+                tt = cls._modify_dss_sparse(key0_ds, key1_ds, val_ds, num_rows_per_append,
+                                            pct_changes if a > 0 else 0.0, num_changes,
+                                            pct_deletes if a > 0 else 0.0, num_deletes,
+                                            pct_inserts if a > 0 else 0.0, num_inserts)
+                tts.append(tt)
+                logger.info('Wrote transaction %d at transaction time %s', a, tt)
+        finally:
+            f.close()
 
 
     @classmethod
@@ -224,27 +224,27 @@ class TestVersionedDatasetPerformance(TestCase):
                                   pct_inserts, num_inserts_0, num_inserts_1):
         logger = logging.getLogger(__name__)
 
-        with temp_dir_ctx() as tmp_dir:
-            filename = tmp_dir + '/test_versioned_dataset_performance.h5'
-            tts = []
-            f = h5py.File(filename, 'w')
-            try:
-                key0_ds = f.create_dataset(name + '/key0', data=np.random.rand(num_rows_initial_0),
-                                           dtype=(np.dtype('int64')), maxshape=(None,), chunks=(int(1e4),))
-                key1_ds = f.create_dataset(name + '/key1', data=np.random.rand(num_rows_initial_1),
-                                           dtype=(np.dtype('int64')), maxshape=(None,), chunks=(int(1e4),))
-                val_ds = f.create_dataset(name + '/val', data=np.random.rand(num_rows_initial_0 * num_rows_initial_1),
-                                          dtype=(np.dtype('float64')), maxshape=(None,), chunks=(int(1e4),))
-                for a in range(num_transactions):
-                    tt = cls._modify_dss_dense(key0_ds, key1_ds, val_ds,
-                                               num_rows_per_append_0,
-                                               pct_changes if a > 0 else 0.0, num_changes,
-                                               pct_deletes if a > 0 else 0.0, num_deletes_0, num_deletes_1,
-                                               pct_inserts if a > 0 else 0.0, num_inserts_0, num_inserts_1)
-                    tts.append(tt)
-                    logger.info('Wrote transaction %d at transaction time %s', a, tt)
-            finally:
-                f.close()
+        tmp_dir = '.'
+        filename = tmp_dir + f'/{name}.h5'
+        tts = []
+        f = h5py.File(filename, 'w')
+        try:
+            key0_ds = f.create_dataset(name + '/key0', data=np.random.rand(num_rows_initial_0),
+                                       dtype=(np.dtype('int64')), maxshape=(None,), chunks=(int(1e4),))
+            key1_ds = f.create_dataset(name + '/key1', data=np.random.rand(num_rows_initial_1),
+                                       dtype=(np.dtype('int64')), maxshape=(None,), chunks=(int(1e4),))
+            val_ds = f.create_dataset(name + '/val', data=np.random.rand(num_rows_initial_0 * num_rows_initial_1),
+                                      dtype=(np.dtype('float64')), maxshape=(None,), chunks=(int(1e4),))
+            for a in range(num_transactions):
+                tt = cls._modify_dss_dense(key0_ds, key1_ds, val_ds,
+                                           num_rows_per_append_0,
+                                           pct_changes if a > 0 else 0.0, num_changes,
+                                           pct_deletes if a > 0 else 0.0, num_deletes_0, num_deletes_1,
+                                           pct_inserts if a > 0 else 0.0, num_inserts_0, num_inserts_1)
+                tts.append(tt)
+                logger.info('Wrote transaction %d at transaction time %s', a, tt)
+        finally:
+            f.close()
 
 
     @classmethod
