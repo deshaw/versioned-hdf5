447593b0fcb7c379809d46efef8c807f28794bc7
Aaron Meurer <asmeurer@gmail.com>
2020-01-16 15:37:47 -0700

    Add some tests for int slicing

--- versioned_hdf5/tests/test_api.py
+++ versioned_hdf5/tests/test_api.py
@@ -46,7 +46,36 @@ def test_stage_version():
         assert_equal(ds[3*CHUNK_SIZE], 0.0)
         assert_equal(ds[3*CHUNK_SIZE+1:4*CHUNK_SIZE], 1.0)
 
-def test_version_slicing():
+def test_version_int_slicing():
+    with setup() as f:
+        file = VersionedHDF5File(f)
+        test_data = np.concatenate((np.ones((2*CHUNK_SIZE,)),
+                               2*np.ones((CHUNK_SIZE,)),
+                               3*np.ones((CHUNK_SIZE,))))
+
+        with file.stage_version('version1', '') as group:
+            group['test_data'] = test_data
+
+        with file.stage_version('version2', 'version1') as group:
+            group['test_data'][0] = 2.0
+
+        with file.stage_version('version3', 'version2') as group:
+            group['test_data'][0] = 3.0
+
+        with file.stage_version('version2_1', 'version1', make_current=False) as group:
+            group['test_data'][0] = 2.0
+
+
+        assert file[0]['test_data'][0] == 3.0
+
+        with raises(ValueError):
+            file[1]
+
+        assert file[-1]['test_data'][0] == 2.0
+        assert file[-2]['test_data'][0] == 1.0, file[-2]
+
+
+def test_version_name_slicing():
     with setup() as f:
         file = VersionedHDF5File(f)
         test_data = np.concatenate((np.ones((2*CHUNK_SIZE,)),
