cd8dbfc2c1ea5a335d793b77661f9cbfd0931757
Aaron Meurer <asmeurer@gmail.com>
2020-01-21 15:22:05 -0700

    Set the default Sphinx role as code
    This lets us use single backticks for code in the docs.

--- docs/conf.py
+++ docs/conf.py
@@ -51,3 +51,6 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Lets us use single backticks for code
+default_role='code'
