03aa590da889ce2464268b251f22ba67d72652d7
Aaron Meurer <asmeurer@gmail.com>
2020-05-05 14:14:51 -0600

    Use ndindex instead of slicetools in backend.py and hashtable.py

--- versioned_hdf5/backend.py
+++ versioned_hdf5/backend.py
@@ -1,4 +1,5 @@
 from h5py import VirtualLayout, VirtualSource
+from ndindex import Slice, ndindex
 
 import math
 
@@ -41,8 +42,6 @@ def create_base_dataset(f, name, *, shape=None, data=None, dtype=None,
 
 def write_dataset(f, name, data, chunk_size=None, compression=None,
                   compression_opts=None):
-    from .slicetools import s2t, t2s
-
     if name not in f['_version_data']:
         return create_base_dataset(f, name, data=data, chunk_size=chunk_size,
             compression=compression, compression_opts=compression_opts)
@@ -66,16 +65,16 @@ def write_dataset(f, name, data, chunk_size=None, compression=None,
     for s in split_chunks(data.shape, chunk_size):
         idx = hashtable.largest_index
         data_s = data[s]
-        raw_slice = slice(idx*chunk_size, idx*chunk_size + data_s.shape[0])
+        raw_slice = Slice(idx*chunk_size, idx*chunk_size + data_s.shape[0])
         data_hash = hashtable.hash(data_s)
         raw_slice2 = hashtable.setdefault(data_hash, raw_slice)
         if raw_slice2 == raw_slice:
-            slices_to_write[s2t(raw_slice)] = s
+            slices_to_write[raw_slice] = s
         slices.append(raw_slice2)
 
     ds.resize((old_shape[0] + len(slices_to_write)*chunk_size,))
     for raw_slice, s in slices_to_write.items():
-        ds[t2s(raw_slice)] = data[s]
+        ds[raw_slice.raw] = data[s]
     return slices
 
 def write_dataset_chunks(f, name, data_dict):
@@ -84,8 +83,6 @@ def write_dataset_chunks(f, name, data_dict):
     array for that chunk, or a slice into the raw data for that chunk
 
     """
-    from .slicetools import s2t, t2s
-
     if name not in f['_version_data']:
         raise NotImplementedError("Use write_dataset() if the dataset does not yet exist")
 
@@ -100,25 +97,25 @@ def write_dataset_chunks(f, name, data_dict):
     slices = [None for i in range(len(data_dict))]
     data_to_write = {}
     for chunk, data_s in data_dict.items():
-        if not isinstance(data_s, (slice, tuple)) and data_s.dtype != ds.dtype:
+        if not isinstance(data_s, (slice, tuple, Slice)) and data_s.dtype != ds.dtype:
             raise ValueError(f"dtypes do not match ({data_s.dtype} != {ds.dtype})")
 
         idx = hashtable.largest_index
-        if isinstance(data_s, (slice, tuple)):
-            slices[chunk] = data_s
+        if isinstance(data_s, (slice, tuple, Slice)):
+            slices[chunk] = ndindex(data_s)
         else:
-            raw_slice = slice(idx*chunk_size, idx*chunk_size + data_s.shape[0])
+            raw_slice = Slice(idx*chunk_size, idx*chunk_size + data_s.shape[0])
             data_hash = hashtable.hash(data_s)
             raw_slice2 = hashtable.setdefault(data_hash, raw_slice)
             if raw_slice2 == raw_slice:
-                data_to_write[s2t(raw_slice)] = data_s
+                data_to_write[raw_slice] = data_s
             slices[chunk] = raw_slice2
 
     assert None not in slices
     old_shape = ds.shape
     ds.resize((old_shape[0] + len(data_to_write)*chunk_size,))
     for raw_slice, data_s in data_to_write.items():
-        ds[t2s(raw_slice)] = data_s
+        ds[raw_slice.raw] = data_s
     return slices
 
 def create_virtual_dataset(f, version_name, name, slices, attrs=None):
@@ -134,7 +131,7 @@ def create_virtual_dataset(f, version_name, name, slices, attrs=None):
 
     for i, s in enumerate(slices):
         # TODO: This needs to handle more than one dimension
-        layout[i*chunk_size:i*chunk_size + s.stop - s.start] = vs[s]
+        layout[i*chunk_size:i*chunk_size + s.stop - s.start] = vs[s.raw]
 
     virtual_data = f['_version_data/versions'][version_name].create_virtual_dataset(name, layout)
 
