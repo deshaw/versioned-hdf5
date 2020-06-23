eac341b05cb33b6712d5a718a42d7f7c2427223a
Aaron Meurer <asmeurer@gmail.com>
2020-06-23 14:09:07 -0600

    Add some tests for InMemorySparseDataset

--- versioned_hdf5/tests/test_wrappers.py
+++ versioned_hdf5/tests/test_wrappers.py
@@ -1,7 +1,7 @@
 import numpy as np
 from numpy.testing import assert_equal
 
-from ..wrappers import InMemoryArrayDataset
+from ..wrappers import InMemoryArrayDataset, InMemorySparseDataset
 
 def test_InMemoryArrayDataset():
     a = np.arange(100,).reshape((50, 2))
@@ -47,3 +47,16 @@ def test_InMemoryArrayDataset_resize():
     assert_equal(dataset, dataset.array)
     assert_equal(dataset, np.arange(90))
     assert dataset.shape == dataset.array.shape == (90,)
+
+def test_InMemorySparseDataset():
+    d = InMemorySparseDataset('data', (1000,), np.float64, fillvalue=1.0)
+    assert d.shape == (1000,)
+    assert d.name == 'data'
+    assert d.dtype == np.float64
+    assert d.fillvalue == np.float64(1.0)
+
+def test_InMemorySparseDataset_getitem():
+    d = InMemorySparseDataset('data', (1000,), np.float64, fillvalue=1.0)
+    assert_equal(d[0], 1.0)
+    assert_equal(d[:], np.ones((1000,)))
+    assert_equal(d[10:20], np.ones((10,)))
