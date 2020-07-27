dad410eee383d2f5dec552adfa10e5326cfa2cef
Aaron Meurer <asmeurer@gmail.com>
2020-07-27 14:25:08 -0600

    Allow closing and reopening the same file in the tests helper

--- versioned_hdf5/tests/helpers.py
+++ versioned_hdf5/tests/helpers.py
@@ -3,10 +3,12 @@ import h5py
 from ..backend import initialize
 
 # TODO: Use a fixture for this
-def setup(file_name='test.hdf5', name=None, version_name=None):
+def setup(file_name='test.hdf5', name=None, version_name=None, init=True):
     # TODO: Use a temporary directory
-    f = h5py.File(file_name, 'w')
-    initialize(f)
+    mode = 'w' if init else 'r+'
+    f = h5py.File(file_name, mode)
+    if init:
+        initialize(f)
     if name:
         f['_version_data'].create_group(name)
     if version_name:
