508b5d0718951483cf9119ddae317212694342a1
Aaron Meurer <asmeurer@gmail.com>
2021-04-07 18:34:14 -0600

    Add a benchmarks for many chunks with arange data

--- benchmarks/many_chunks.py
+++ benchmarks/many_chunks.py
@@ -45,3 +45,16 @@ def time_many_chunks_integer_index():
             i2 = np.random.choice(d1, 30, replace=False)
             i2 = np.sort(i2)
             sv['bar'][:, i2, :] = np.full((d0, len(i2), d2), i, dtype=dt)
+
+def time_many_chunks_arange():
+    d0 = 2
+    d1 = 15220
+    d2 = 2
+    shape = (d0, d1, d2)
+    chunks = (600, 2, 4)
+    with h5py.File('foo.h5', 'w') as f:
+        vf = VersionedHDF5File(f)
+        with vf.stage_version('0') as sv:
+            sv.create_dataset('bar', shape=shape, maxshape=(None, None, None),
+                              chunks=chunks, dtype=dt,
+                              data=np.arange(np.prod(shape), dtype=dt).reshape(shape))
