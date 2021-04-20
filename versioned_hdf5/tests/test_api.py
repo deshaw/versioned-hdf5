753dbeef79610be9e87f52cc245f08c220e647d4
Aaron Meurer <asmeurer@gmail.com>
2021-04-20 13:22:50 -0600

    Don't close the file in the test itself
    The fixture does this.

--- versioned_hdf5/tests/test_api.py
+++ versioned_hdf5/tests/test_api.py
@@ -581,14 +581,6 @@ def test_resize_multiple_dimensions(tmp_path, h5file):
         assert version3_2[f'dataset3_{i}'].shape == newshape
         assert_equal(version3_2[f'dataset3_{i}'][()], new_data)
 
-    vfile.close()
-    try:
-        h5file.close()
-    except ValueError:
-        # Work around a bug in h5py. See
-        # https://github.com/deshaw/versioned-hdf5/pull/125
-        pass
-
 def test_getitem(vfile):
     data = np.arange(2*DEFAULT_CHUNK_SIZE)
 
