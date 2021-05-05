c0e13d6850b0652af4363b05bdb789aa5b9b96a8
Aaron Meurer <asmeurer@gmail.com>
2021-05-05 14:25:08 -0600

    Add a test for delete_version()

--- versioned_hdf5/tests/test_replay.py
+++ versioned_hdf5/tests/test_replay.py
@@ -1,8 +1,8 @@
 import numpy as np
 
-from versioned_hdf5.replay import modify_metadata
+from versioned_hdf5.replay import modify_metadata, delete_version
 
-def setup_modify_metadata(file):
+def setup_vfile(file):
     with file.stage_version('version1') as g:
         data = g.create_dataset('test_data', data=None, fillvalue=1., shape=(10000,), chunks=(1000,))
         data[0] = 0.
@@ -12,28 +12,33 @@ def setup_modify_metadata(file):
         g['test_data'][2000] = 2.
         g.create_dataset('test_data3', data=[1, 2, 3, 4], chunks=(1000,))
 
-def check_modify_metadata_data(file, test_data_fillvalue=1.):
+def check_data(file, test_data_fillvalue=1., version2=True):
     assert file['version1']['test_data'].shape == (10000,)
     assert file['version1']['test_data'][0] == 0.
     assert np.all(file['version1']['test_data'][1:] == test_data_fillvalue)
 
-    assert file['version2']['test_data'].shape == (10000,)
-    assert file['version2']['test_data'][0] == 0.
-    assert np.all(file['version2']['test_data'][1:2000] == test_data_fillvalue)
-    assert file['version2']['test_data'][2000] == 2.
-    assert np.all(file['version2']['test_data'][2001:] == test_data_fillvalue)
+    if version2:
+        assert file['version2']['test_data'].shape == (10000,)
+        assert file['version2']['test_data'][0] == 0.
+        assert np.all(file['version2']['test_data'][1:2000] == test_data_fillvalue)
+        assert file['version2']['test_data'][2000] == 2.
+        assert np.all(file['version2']['test_data'][2001:] == test_data_fillvalue)
 
     assert file['version1']['test_data2'].shape == (3,)
     assert np.all(file['version1']['test_data2'][:] == [1, 2, 3])
-    assert file['version2']['test_data2'].shape == (3,)
-    assert np.all(file['version2']['test_data2'][:] == [1, 2, 3])
+
+    if version2:
+        assert file['version2']['test_data2'].shape == (3,)
+        assert np.all(file['version2']['test_data2'][:] == [1, 2, 3])
 
     assert 'test_data3' not in file['version1']
-    assert file['version2']['test_data3'].shape == (4,)
-    assert np.all(file['version2']['test_data3'][:] == [1, 2, 3, 4])
+
+    if version2:
+        assert file['version2']['test_data3'].shape == (4,)
+        assert np.all(file['version2']['test_data3'][:] == [1, 2, 3, 4])
 
 def test_modify_metadata_compression(vfile):
-    setup_modify_metadata(vfile)
+    setup_vfile(vfile)
 
     f = vfile.f
 
@@ -59,7 +64,7 @@ def test_modify_metadata_compression(vfile):
     assert f['_version_data']['test_data3']['raw_data'].compression_opts == None
 
     modify_metadata(f, 'test_data2', compression='gzip', compression_opts=3)
-    check_modify_metadata_data(vfile)
+    check_data(vfile)
 
     assert vfile['version1']['test_data'].compression == None
     assert vfile['version2']['test_data'].compression == None
@@ -86,7 +91,7 @@ def test_modify_metadata_compression(vfile):
     assert list(f['_version_data']) == ['test_data', 'test_data2', 'test_data3', 'versions']
 
 def test_modify_metadata_chunks(vfile):
-    setup_modify_metadata(vfile)
+    setup_vfile(vfile)
 
     f = vfile.f
 
@@ -103,7 +108,7 @@ def test_modify_metadata_chunks(vfile):
     assert f['_version_data']['test_data3']['raw_data'].chunks == (1000,)
 
     modify_metadata(f, 'test_data2', chunks=(500,))
-    check_modify_metadata_data(vfile)
+    check_data(vfile)
 
     assert vfile['version1']['test_data'].chunks == (1000,)
     assert vfile['version2']['test_data'].chunks == (1000,)
@@ -121,7 +126,7 @@ def test_modify_metadata_chunks(vfile):
     assert list(f['_version_data']) == ['test_data', 'test_data2', 'test_data3', 'versions']
 
 def test_modify_metadata_dtype(vfile):
-    setup_modify_metadata(vfile)
+    setup_vfile(vfile)
 
     f = vfile.f
 
@@ -138,7 +143,7 @@ def test_modify_metadata_dtype(vfile):
     assert f['_version_data']['test_data3']['raw_data'].dtype == np.int64
 
     modify_metadata(f, 'test_data2', dtype=np.float64)
-    check_modify_metadata_data(vfile)
+    check_data(vfile)
 
 
     assert vfile['version1']['test_data'].dtype == np.float64
@@ -157,7 +162,7 @@ def test_modify_metadata_dtype(vfile):
     assert list(f['_version_data']) == ['test_data', 'test_data2', 'test_data3', 'versions']
 
 def test_modify_metadata_fillvalue1(vfile):
-    setup_modify_metadata(vfile)
+    setup_vfile(vfile)
 
     f = vfile.f
 
@@ -174,7 +179,7 @@ def test_modify_metadata_fillvalue1(vfile):
     assert f['_version_data']['test_data3']['raw_data'].fillvalue == 0
 
     modify_metadata(f, 'test_data', fillvalue=3.)
-    check_modify_metadata_data(vfile, test_data_fillvalue=3.)
+    check_data(vfile, test_data_fillvalue=3.)
 
     assert vfile['version1']['test_data'].fillvalue == 3.
     assert vfile['version2']['test_data'].fillvalue == 3.
@@ -192,7 +197,7 @@ def test_modify_metadata_fillvalue1(vfile):
     assert list(f['_version_data']) == ['test_data', 'test_data2', 'test_data3', 'versions']
 
 def test_modify_metadata_fillvalue2(vfile):
-    setup_modify_metadata(vfile)
+    setup_vfile(vfile)
 
     f = vfile.f
 
@@ -209,7 +214,7 @@ def test_modify_metadata_fillvalue2(vfile):
     assert f['_version_data']['test_data3']['raw_data'].fillvalue == 0
 
     modify_metadata(f, 'test_data2', fillvalue=3)
-    check_modify_metadata_data(vfile)
+    check_data(vfile)
 
     assert vfile['version1']['test_data'].fillvalue == 1.
     assert vfile['version2']['test_data'].fillvalue == 1.
@@ -225,3 +230,13 @@ def test_modify_metadata_fillvalue2(vfile):
 
     # Make sure the tmp group group has been destroyed.
     assert list(f['_version_data']) == ['test_data', 'test_data2', 'test_data3', 'versions']
+
+def test_delete_version(vfile):
+    setup_vfile(vfile)
+    f = vfile.f
+
+    delete_version(f, 'version2')
+    check_data(vfile, version2=False)
+    assert list(vfile) == ['version1']
+    assert list(f['_version_data']) == ['test_data', 'test_data2', 'versions']
+    assert not np.isin(2., f['_version_data']['test_data']['raw_data'][:])
