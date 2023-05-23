add0122a0fe5556e6d4da23e7550f3b3db42ea20
pdmurray <peynmurray@gmail.com>
2023-05-23 11:11:38 -0700

    Fix InMemoryDataset.__getitem__ to work with development version of h5py

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setuptools.setup(
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.8',
     extras_require={
         "test": [
             "pytest",
