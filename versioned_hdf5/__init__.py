4b5833cb7edabf73950954b1d82e6974344f116d
pdmurray <peynmurray@gmail.com>
2024-01-19 12:48:16 -0800

    Update the build system to use meson-python; add 3.12 compatibility

--- versioned_hdf5/__init__.py
+++ versioned_hdf5/__init__.py
@@ -2,7 +2,3 @@ from .api import VersionedHDF5File
 from .replay import delete_version, delete_versions, modify_metadata
 
 __all__ = ['VersionedHDF5File', 'delete_version', 'delete_versions', 'modify_metadata']
-
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
