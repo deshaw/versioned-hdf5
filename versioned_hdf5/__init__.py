ed49f577ff11a9a295de2a9442a00f65ef1e05a5
Peyton Murray <peynmurray@gmail.com>
2024-06-19 15:14:35 -0700

    Add version dunder back in using importlib.metadata (#344)

--- versioned_hdf5/__init__.py
+++ versioned_hdf5/__init__.py
@@ -1,4 +1,8 @@
+from importlib.metadata import version
+
 from .api import VersionedHDF5File
 from .replay import delete_version, delete_versions, modify_metadata
+
+__version__ = version(__package__)
 
 __all__ = ["VersionedHDF5File", "delete_version", "delete_versions", "modify_metadata"]
