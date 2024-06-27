29fe8ce5944f684a737ce0043b0a8e9e4711ae69
ArvidJB <ArvidJB@users.noreply.github.com>
2024-06-27 18:18:09 -0400

    Move slicetools implementation to cython (PyInf#12655) (#346)
    Constructing `data_dict` is pretty slow:
    
    ```
    In [2]: with h5py.File('data.h5', 'w') as f:
       ...:     vf = VersionedHDF5File(f)
       ...:     with vf.stage_version('v0') as sv:
       ...:         sv.create_dataset('values', data=np.arange(321 * 54321).reshape((321, 54321)), chunks=(10, 100), maxshape=(None, None))
       ...:
    [PYFLYBY] import numpy as np
    
    In [4]: %%pyinstrument
       ...: with h5py.File('data.h5', 'r+') as f:
       ...:     vf = VersionedHDF5File(f)
       ...:     with vf.stage_version('v1') as sv:
       ...:         values = sv['values']
       ...:         values.resize((432, 65432))
       ...:
    
      _     ._   __/__   _ _  _  _ _/_   Recorded: 11:00:01  Samples:  3915
     /_//_/// /_\ / //_// / //_'/ //     Duration: 6.845     CPU time: 6.262
    /   _/                      v4.6.1
    
    Program: /usr/local/bin/py
    
    6.844 <module>  <ipython-input-4-25703e99d0a0>:1
    |- 3.034 InMemoryDataset.resize  versioned_hdf5/wrappers.py:609
    |  |- 1.292 InMemoryDatasetID.data_dict  versioned_hdf5/wrappers.py:1298
    |  |  |- 0.690 <dictcomp>  versioned_hdf5/wrappers.py:1327
    |  |  |  |- 0.600 spaceid_to_slice  versioned_hdf5/slicetools.py:6
    |  |  |  |  `- 0.506 [self]  versioned_hdf5/slicetools.py
    |  |  |  `- 0.090 [self]  versioned_hdf5/wrappers.py
    |  |  |- 0.317 <listcomp>  versioned_hdf5/wrappers.py:1317
    |  |  |  `- 0.301 [self]  versioned_hdf5/wrappers.py
    |  |  `- 0.211 [self]  versioned_hdf5/wrappers.py
    |  |- 0.951 InMemoryDataset.get_index  versioned_hdf5/wrappers.py:655
    |  |  `- 0.951 InMemoryDataset.__getitem__  h5py/_hl/dataset.py:749
    |  |     `- 0.949 Reader.read  <built-in>
    |  `- 0.677 [self]  versioned_hdf5/wrappers.py
    |- 2.800 _GeneratorContextManager.__exit__  contextlib.py:141
    |  `- 2.800 VersionedHDF5File.stage_version  versioned_hdf5/api.py:267
    ...
    |- 0.762 _GeneratorContextManager.__enter__  contextlib.py:132
    |  `- 0.762 VersionedHDF5File.stage_version  versioned_hdf5/api.py:267
    ...
    `- 0.234 File.__exit__  h5py/_hl/files.py:601
       `- 0.234 File.close  h5py/_hl/files.py:576
    ```
    
    Most of this is simply overhead. By re-implementing the building of the data_dict
    in Cython we can speed up the performance 40x-50x:
    
    ```
    |- 0.509 InMemoryDataset.resize  versioned_hdf5/wrappers.py:608
    |  |- 0.265 InMemoryDataset.get_index  versioned_hdf5/wrappers.py:654
    |  |  `- 0.264 InMemoryDataset.__getitem__  h5py/_hl/dataset.py:749
    |  |     `- 0.264 Reader.read  <built-in>
    |  |- 0.154 [self]  versioned_hdf5/wrappers.py
    |  `- 0.030 InMemoryDatasetID.data_dict  versioned_hdf5/wrappers.py:1297
    ```

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,8 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+# import versioned_hdf5 from compiled and installed extension
+# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 
 # -- Project information -----------------------------------------------------
