a226563710610c920024f2b54d266124b85a45ad
crusaderky <crusaderky@gmail.com>
2024-11-19 12:36:39 +0000

    Debug libhdf5 version in CI

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,6 @@
-from __future__ import absolute_import, division, print_function, with_statement
-
+import os
 import shutil
+import subprocess
 import tempfile
 from contextlib import contextmanager
 
@@ -10,3 +10,17 @@ def temp_dir_ctx():
     tmp_dir = tempfile.mkdtemp()
     yield tmp_dir
     shutil.rmtree(tmp_dir)
+
+
+def debug_libhdf5_so():
+    """Print out which .so file is actually loaded at runtime for libhdf5"""
+    import h5py
+
+    for row in (
+        subprocess.check_output(["lsof", "-p", str(os.getpid())])
+        .decode("utf-8")
+        .splitlines()
+    ):
+        row = row.strip()
+        if "libhdf5" in row:
+            print(row)
