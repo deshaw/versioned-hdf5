f72c8cfe9654d0d73acb2d633cc8e180d285e637
crusaderky <crusaderky@gmail.com>
2025-06-06 10:53:54 +0100

    remove commented out code from conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,6 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 
-# import os
-# import sys
-# import versioned_hdf5 from compiled and installed extension
 # sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 # -- Project information -----------------------------------------------------
